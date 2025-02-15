41b9ba92cd0fd529bd7845cfa9100c5781624c6b
Rangga <rangaaa.pratamaaa@gmail.com>
2025-02-15 15:17:39 +0700

    blacklist token

--- app/configs/cache.ts
+++ app/configs/cache.ts
@@ -2,14 +2,9 @@ import env from "~/configs/env";
 
 // Configuration for cache system
 
-export const cacheDriver = env.CACHE_DRIVER || "file";
+export const cacheDriver = env.CACHE_DRIVER;
 
 export const cacheConfig = {
-    file: {
-        driver: "file",
-        storagePath: "storages/cache/cache.json"
-    },
-
     redis: {
         driver: "redis",
         mode: "standalone",
--- app/src/services/cacheService.ts
+++ app/src/services/cacheService.ts
@@ -2,35 +2,7 @@ import fs from "fs";
 import path from "path";
 import env from "~/configs/env";
 import redis, { RedisClientType } from "redis";
-import { cacheConfig, cacheDriver } from "~/configs/cache.js";
-
-//     FILE     ---------------------------------------------------------
-
-const filePath = path.resolve(cacheConfig.file.storagePath);
-
-if (!fs.existsSync(filePath)) {
-    fs.writeFileSync(filePath, JSON.stringify({}));
-}
-
-class fileCache {
-    get(key: string) {
-        const cache = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-        if (cache[key].expiresAt < Date.now()) delete cache[key];
-        return cache[key] ?? null;
-    }
-    set(key: string, value: unknown, ttl: number) {
-        const cache = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-        cache[key] = { value, expiresAt: Date.now() + ttl * 1000 };
-        fs.writeFileSync(filePath, JSON.stringify(cache));
-    }
-    del(key: string) {
-        const cache = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-        delete cache[key];
-        fs.writeFileSync(filePath, JSON.stringify(cache));
-    }
-}
-
-//     REDIS     ---------------------------------------------------------
+import { cacheDriver } from "~/configs/cache.js";
 
 export let redisClient: RedisClientType;
 
@@ -70,17 +42,6 @@ class redisCache {
 
 //     CACHE     ---------------------------------------------------------
 
-let cache: fileCache | redisCache;
-
-switch (cacheDriver) {
-    case "file":
-        cache = new fileCache();
-        break;
-    case "redis":
-        cache = new redisCache();
-        break;
-    default:
-        throw new Error(`Unsupported cache driver: ${cacheDriver}`);
-}
+const cache = new redisCache();
 
 export default cache;
